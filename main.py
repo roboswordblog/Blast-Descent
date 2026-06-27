9d4005c58adec1d1289906db73b221e4d81d4d14
robosword <robotanmol@gmail.com>
2026-06-27 00:26:18 -0700

    Fixing the balloons positions and doing things to make my framreate NOT crash

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from particles import *
+from player import *
+from bullets import *
+from enemy import *
+from ui import *
 import pygame
 pygame.init()
 
 window = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Sky Fighter")
 
+class States:
+    def __init__(self):
+        pass
+player = Player(500, 200)
 while True:
     window.fill((200, 220, 226))
     for particle in particleList:
         particle.draw(window)
         particle.update()
+    player.draw(window)
+    player.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
--- player.py
+++ player.py
@@ -4,41 +4,49 @@ import math
 from bullets import *
 balloons = 3
 class Balloon:
-    def __init__(self, type,num, player):
+    def __init__(self, type, num, player):
         self.type = type
         self.player = player
         self.num = num
-        self.image = pygame.transform.rotate(pygame.image.load(f"assets/balloons/{self.type}.png"), (num*30)-30)
-        self.rect = self.image.get_rect()
+
+        self.base_image = pygame.image.load(f"assets/balloons/{self.type}.png")
+        self.rect = self.base_image.get_rect()
+
         self.dead = False
+
+        # IMPORTANT: initialize position immediately
+        self.rect.midbottom = self.player.rect.midtop
+        self.rect.y -= (self.num * 12)
 
     def draw(self, window):
         if self.dead:
             return
-        base_image = pygame.image.load(f"assets/balloons/{self.type}.png")
-        wobble = math.sin(pygame.time.get_ticks() * 0.005 + self.num) * 3
-        angle = (self.num * 30) - 30 + wobble
 
-        image = pygame.transform.rotate(base_image, angle)
+        wobble = math.sin(pygame.time.get_ticks() * 0.005 + self.num) * 3
+        angle = 0 + wobble
+
+        image = pygame.transform.rotate(self.base_image, angle)
 
         rect = image.get_rect(center=self.rect.center)
-
         window.blit(image, rect)
 
     def update(self):
         if self.dead:
             return
-        global balloons
-        self.rect.x = self.player.x
-        self.rect.y = self.player.y + 10
+
+        base_x = self.player.rect.centerx
+        base_y = self.player.rect.top
+
+        self.rect.centerx = base_x
+        self.rect.centery = base_y - (self.num * 18)
+
         for bullet in bulletList:
             if self.rect.colliderect(bullet.rect):
                 if bullet.type == "enemy":
                     for i in range(10):
-                        Particle(self.rect.x, self.rect.y, (180, 180, 180))
-                        balloons -= 1
-                        self.dead = True
+                        Particle(self.rect.centerx, self.rect.centery, (200, 200, 200))
 
+                    self.dead = True
 
 class Player:
     def __init__(self, x, y):
@@ -51,11 +59,17 @@ class Player:
         self.firingCooldown = 0
         self.reloadCooldown = 0
         # reload time should be 4 seconds
-        self.image = pygame.image.load(f"images/player.{self.mode}.png")
+        self.image = pygame.transform.scale(pygame.image.load(f"assets/player/{self.mode}.png"), (64, 48))
         self.rect = self.image.get_rect()
         self.balloon1 = Balloon("0", 0, self)
         self.balloon2 = Balloon("1", 1, self)
         self.balloon3 = Balloon("2", 2, self)
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.imageDict = {
+            "idle": pygame.transform.scale(pygame.image.load(f"assets/player/idle.png"), (64, 48)),
+            "reload": pygame.transform.scale(pygame.image.load(f"assets/player/reload.png"), (64, 48))
+        }
 
     def update(self):
         self.rect.x = self.x
@@ -64,8 +78,11 @@ class Player:
             self.mode = "reload"
         self.balloon1.update()
         self.balloon2.update()
-
+        self.balloon3.update()
 
     def draw(self, window):
-        self.image = pygame.image.load(f"images/player.{self.mode}.png")
+        self.image = self.imageDict[self.mode]
+        self.balloon1.draw(window)
+        self.balloon2.draw(window)
+        self.balloon3.draw(window)
         window.blit(self.image, self.rect)
