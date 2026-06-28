56e97e6a2411b66e4e82c467ce3bc816275ae814
robosword <robotanmol@gmail.com>
2026-06-27 22:06:14 -0700

    Make the cloud class

---
+++ clouds.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+cloudList = []
+class Cloud:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(f"assets/clouds/{random.choice([0,1])}.png")
+
+    def update(self):
+        self.x -= 1
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
