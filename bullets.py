bbaf1bd9b2a11863a9ec9a6303c84b97467f9b6c
robosword <robotanmol@gmail.com>
2026-06-28 23:34:29 -0700

    Adding the particless

--- bullets.py
+++ bullets.py
@@ -1,4 +1,5 @@
 import pygame
+from particles import *
 bulletList = []
 
 class Bullet:
@@ -12,10 +13,26 @@ class Bullet:
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+        self.timer = 100
         bulletList.append(self)
 
     def update(self):
         self.x -= self.dir
+        self.timer -= 1
+        if self.timer <= 0:
+            for i in range(50):
+                Particle(
+                    self.x,
+                    self.y,
+                    random.choice([
+                        (120, 120, 120),
+                        (180, 180, 180),
+                        (255, 60, 60),
+                        (200, 30, 30),
+                        (255, 140, 0),
+                        (255, 180, 40),
+                    ])
+                )
 
-    def draw(self):
-        pass
+    def draw(self, window):
+        window.blit(self.image, self.rect)
