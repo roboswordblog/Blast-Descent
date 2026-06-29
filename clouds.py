75937ac630cde6d204da4d82bf086b962e28710b
robosword <robotanmol@gmail.com>
2026-06-28 22:11:22 -0700

    Make the cloud class

--- clouds.py
+++ clouds.py
@@ -6,10 +6,9 @@ class Cloud:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load(f"assets/clouds/{random.choice([0,1])}.png")
+        self.image = pygame.transform.scale2x(pygame.transform.scale2x(pygame.image.load(f"assets/clouds/{random.choice([0,1])}.png")))
 
     def update(self, player):
-        self.x += 0.5
         self.y += player.ay
 
 
