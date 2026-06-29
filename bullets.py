21b25f5ceb4502fb088101ae4a167513b611e674
robosword <robotanmol@gmail.com>
2026-06-28 23:23:00 -0700

    Make the bullets class

--- bullets.py
+++ bullets.py
@@ -7,3 +7,15 @@ class Bullet:
         self.y = y
         self.dir = dir
         self.tag = tag
+        self.image = pygame.transform.flip(pygame.transform.scale2x(pygame.image.load(f"assets/bullets/{tag}.png")), dir, False)
+        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        bulletList.append(self)
+
+    def update(self):
+        self.x -= self.dir
+
+    def draw(self):
+        pass
